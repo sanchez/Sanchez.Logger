d755bc04e8bfe5477518c74523bdd63b463c9c81
sanchez <dfitz360@gmail.com>
2020-02-12 20:59:55 +1000

    Logs are output to file in json form

--- Sanchez.Logger.Sinks.File/FileSink.fs
+++ Sanchez.Logger.Sinks.File/FileSink.fs
@@ -1,5 +1,6 @@
 ﻿module Sanchez.Logger.Sinks.File
 
+open FSharp.Json
 open System
 open System.IO
 open Sanchez.Logger.Abstraction
@@ -8,15 +9,32 @@ type FileConfiguration =
     {
         fileLocation: string option
     }
+    
+let private renderColor (color: LogColouring) =
+    match color with
+    | Red s | Green s | Yellow s | Blue s | Magenta s | Cyan s | Faded s -> s
+    | Reset -> ""
+    
+let private render (symbol: LogSymbols) =
+    match symbol with
+    | Coloring c -> renderColor c
+    | Message msg -> msg
+    
+let private handleLogMessage (writer: StreamWriter) (providers: ProviderMap) (message: string) =
+    let line = 
+        providers
+        |> Map.map (fun _ -> render)
+        |> Map.add "message" message
+        |> Json.serialize
+        
+    writer.WriteLine line
+    writer.Flush ()
 
 let Sink (fileConfig: FileConfiguration) =
     let fileName = fileConfig.fileLocation |> Option.defaultWith Path.GetTempFileName
-    use writer = File.AppendText fileName
+    let writer = File.AppendText fileName
     
     if fileConfig.fileLocation |> Option.isNone then
         printfn "Log File Output Location: %s" fileName
     
-    let handler (providers: ProviderMap) (message: string) =
-        ()
-        
-    handler
+    handleLogMessage writer
